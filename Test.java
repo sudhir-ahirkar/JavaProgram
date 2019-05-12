395c59657c112305d40fc482b4a4b817c4705a55
sudhir-ahirkar <sudhir.ahirkar28@gmail.com>
2019-05-12 18:48:15 +0530

    added acontent to file

--- Test.java
+++ Test.java
@@ -3,6 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Test Application");
+		
+		System.out.println("Testing SCM poll");
 	}
 
 }
